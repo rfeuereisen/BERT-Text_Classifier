319aea215a973c1450f1d022fb26b484c6dc9a68
Rosario Feuereisen <112586014+rfeuereisen@users.noreply.github.com>
2023-12-06 09:51:15 -0300

    Update inference.py

--- entry_points/inference.py
+++ entry_points/inference.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import sys
 import os
 import pandas as pd
+from tkinter import Tk, filedialog, simpledialog
 
 project_root = Path(__file__).resolve().parent.parent
 sys.path.append(str(project_root))
@@ -48,7 +49,40 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL_CKPT_NIVEL_TIPO_DETENCION)
 model_tipo_detencion = AutoModelForSequenceClassification.from_pretrained(MODEL_CKPT_NIVEL_TIPO_DETENCION).to(device)
 
 
+def select_excel_and_sheet():
+    root = Tk()
+    root.withdraw()
 
+    # Seleccionar el archivo de Excel
+    excel_file_path = filedialog.askopenfilename(title="Selecciona el archivo de Excel",
+                                                 filetypes=[("Excel files", "*.xlsx;*.xls")])
+
+    if not excel_file_path:
+        print("No se seleccionó un archivo. Saliendo.")
+        exit()
+
+    # Leer las pestañas disponibles
+    df = pd.read_excel(excel_file_path, sheet_name=None)
+    sheet_names = list(df.keys())
+
+    if not sheet_names:
+        print("El archivo de Excel no contiene pestañas. Saliendo.")
+        exit()
+
+    # Seleccionar la pestaña
+    sheet_name = simpledialog.askstring("Selecciona la pestaña de Excel", "Pestañas disponibles: " + ", ".join(sheet_names))
+
+    if sheet_name not in sheet_names:
+        print("Pestaña no válida. Saliendo.")
+        exit()
+
+    return excel_file_path, sheet_name
+
+def save_results():
+    root = Tk()
+    root.withdraw()
+    file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx;*.xls")])
+    return file_path
 
 def inference(text: str, model) -> str:
     "Predict the category of a given text"
@@ -65,7 +99,11 @@ def inference(text: str, model) -> str:
 
 
 if __name__ == "__main__":
-    df = pd.read_excel("data/prueba2.xlsx", sheet_name='BD 2')
+
+    # Seleccionar el archivo de entrada y la pestaña de Excel
+    excel_file_path, sheet_name = select_excel_and_sheet()
+
+    df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
     colum_texts = list(df['Datos BI'])
 
 
@@ -73,37 +111,14 @@ if __name__ == "__main__":
 
         numero_reason = df.loc[idx, 'Número Reason']
         print(f"Procesando índice {idx}: Número Reason = {numero_reason}")
+
         # Condición 1: '3000'
         if str(df.loc[idx, 'Número Reason']) == '3000':
             print("Condición 1 se cumple")
-            df.loc[idx, 'Nivel_1'] = 'PM'
-            df.loc[idx, 'Nivel_2'] = 'PM'
-            df.loc[idx, 'Nivel_3'] = None
-            df.loc[idx, 'Comentario'] = 'PM'
-            df.loc[idx, 'Tipo de Detención'] = 'MP'
-
-        # Condición 2: '3030'
-        if str(df.loc[idx, 'Número Reason']) == '3030':
-            print("Condición 2 se cumple")
-            df.loc[idx, 'Nivel_1'] = 'NEUMATICOS_Y_AROS'
-            df.loc[idx, 'Nivel_2'] = 'NEUMATICOS_Y_AROS'
-            df.loc[idx, 'Nivel_3'] = None
-            df.loc[idx, 'Comentario'] = 'PM'
-            df.loc[idx, 'Tipo de Detención'] = 'MPV'
-
-        # Condición 3: '5030'
-        if str(df.loc[idx, 'Número Reason']) == '5030':
-            print("Condición 3 se cumple")
-            df.loc[idx, 'Nivel_1'] = 'NEUMATICOS_Y_AROS'
-            df.loc[idx, 'Nivel_2'] = 'NEUMATICOS_Y_AROS'
-            df.loc[idx, 'Nivel_3'] = None
             category_comentario = inference(str(text), model_comentario)
+            if category_comentario == "VACIO":
+                category_comentario = ""
             df.loc[idx, 'Comentario'] = category_comentario
-            df.loc[idx, 'Tipo de Detención'] = 'MCV'
-
-        # Condición 4: '5010'
-        if str(df.loc[idx, 'Número Reason']) == '5010':
-            print("Condición 4 se cumple")
             category_nivel_1 = inference(str(text), model_nivel_1)
             if category_nivel_1 == "VACIO":
                 category_nivel_1 = ""
@@ -116,32 +131,99 @@ if __name__ == "__main__":
             if category_nivel_3 == "VACIO":
                 category_nivel_3 = ""
             df.loc[idx, 'Nivel_3'] = category_nivel_3
+            category_tipo = inference(str(text), model_tipo_detencion)
+            if category_tipo == "VACIO":
+                category_tipo = ""
+            df.loc[idx, 'Tipo de Detención'] = category_tipo
+        
+         # Condición 2: '3010'
+        if str(df.loc[idx, 'Número Reason']) == '3010':
+            print("Condición 2 se cumple")
             category_comentario = inference(str(text), model_comentario)
+            if category_comentario == "VACIO":
+                category_comentario = ""
             df.loc[idx, 'Comentario'] = category_comentario
+            category_nivel_1 = inference(str(text), model_nivel_1)
+            if category_nivel_1 == "VACIO":
+                category_nivel_1 = ""
+            df.loc[idx, 'Nivel_1'] = category_nivel_1
+            category_nivel_2 = inference(str(text), model_nivel_2)
+            if category_nivel_2 == "VACIO":
+                category_nivel_2 = ""
+            df.loc[idx, 'Nivel_2'] = category_nivel_2
+            category_nivel_3 = inference(str(text), model_nivel_3)
+            if category_nivel_3 == "VACIO":
+                category_nivel_3 = ""
+            df.loc[idx, 'Nivel_3'] = category_nivel_3
+            category_tipo = inference(str(text), model_tipo_detencion)
+            if category_tipo == "VACIO":
+                category_tipo = ""
+            df.loc[idx, 'Tipo de Detención'] = category_tipo
+
+        # Condición 3: '3030'
+        if str(df.loc[idx, 'Número Reason']) == '3030':
+            print("Condición 3 se cumple")
+            category_comentario = inference(str(text), model_comentario)
+            if category_comentario == "VACIO":
+                category_comentario = ""
+            df.loc[idx, 'Comentario'] = category_comentario
+            df.loc[idx, 'Nivel_1'] = 'NEUMATICOS'
+            df.loc[idx, 'Nivel_2'] = 'NEUMATICOS_Y_AROS'
+            df.loc[idx, 'Nivel_3'] = None
+            df.loc[idx, 'Tipo de Detención'] = "MPV"
+
+        # Condición 4: '5000'
+        if str(df.loc[idx, 'Número Reason']) == '5000':
+            print("Condición 4 se cumple")
+            category_comentario = inference(str(text), model_comentario)
+            if category_comentario == "VACIO":
+                category_comentario = ""
+            df.loc[idx, 'Comentario'] = category_comentario
+            category_nivel_1 = inference(str(text), model_nivel_1)
+            if category_nivel_1 == "VACIO":
+                category_nivel_1 = ""
+            df.loc[idx, 'Nivel_1'] = category_nivel_1
+            category_nivel_2 = inference(str(text), model_nivel_2)
+            if category_nivel_2 == "VACIO":
+                category_nivel_2 = ""
+            df.loc[idx, 'Nivel_2'] = category_nivel_2
+            category_nivel_3 = inference(str(text), model_nivel_3)
+            if category_nivel_3 == "VACIO":
+                category_nivel_3 = ""
+            df.loc[idx, 'Nivel_3'] = category_nivel_3
+            category_tipo = inference(str(text), model_tipo_detencion)
+            if category_tipo == "VACIO":
+                category_tipo = ""
+            df.loc[idx, 'Tipo de Detención'] = category_tipo
+
+        # Condición 5: '5010'
+        if str(df.loc[idx, 'Número Reason']) == '5010':
+            print("Condición 5 se cumple")
+            category_comentario = inference(str(text), model_comentario)
+            if category_comentario == "VACIO":
+                category_comentario = ""
+            df.loc[idx, 'Comentario'] = category_comentario
+            category_nivel_1 = inference(str(text), model_nivel_1)
+            if category_nivel_1 == "VACIO":
+                category_nivel_1 = ""
+            df.loc[idx, 'Nivel_1'] = category_nivel_1
+            category_nivel_2 = inference(str(text), model_nivel_2)
+            if category_nivel_2 == "VACIO":
+                category_nivel_2 = ""
+            df.loc[idx, 'Nivel_2'] = category_nivel_2
+            category_nivel_3 = inference(str(text), model_nivel_3)
+            if category_nivel_3 == "VACIO":
+                category_nivel_3 = ""
+            df.loc[idx, 'Nivel_3'] = category_nivel_3
             df.loc[idx, 'Tipo de Detención'] = 'MCA'
 
-        # Condición 5: '5020'
+         # Condición 6: '5020'
         if str(df.loc[idx, 'Número Reason']) == '5020':
-            print("Condición 5 se cumple")
-            category_nivel_1 = inference(str(text), model_nivel_1)
-            if category_nivel_1 == "VACIO":
-                category_nivel_1 = ""
-            df.loc[idx, 'Nivel_1'] = category_nivel_1
-            category_nivel_2 = inference(str(text), model_nivel_2)
-            if category_nivel_2 == "VACIO":
-                category_nivel_2 = ""
-            df.loc[idx, 'Nivel_2'] = category_nivel_2
-            category_nivel_3 = inference(str(text), model_nivel_3)
-            if category_nivel_3 == "VACIO":
-                category_nivel_3 = ""
-            df.loc[idx, 'Nivel_3'] = category_nivel_3
-            category_comentario = inference(str(text), model_comentario)
-            df.loc[idx, 'Comentario'] = category_comentario
-            df.loc[idx, 'Tipo de Detención'] = 'MCI'
-
-        # Condición 6: '5050'
-        if str(df.loc[idx, 'Número Reason']) == '5050':
             print("Condición 6 se cumple")
+            category_comentario = inference(str(text), model_comentario)
+            if category_comentario == "VACIO":
+                category_comentario = ""
+            df.loc[idx, 'Comentario'] = category_comentario
             category_nivel_1 = inference(str(text), model_nivel_1)
             if category_nivel_1 == "VACIO":
                 category_nivel_1 = ""
@@ -154,13 +236,51 @@ if __name__ == "__main__":
             if category_nivel_3 == "VACIO":
                 category_nivel_3 = ""
             df.loc[idx, 'Nivel_3'] = category_nivel_3
+            category_tipo = inference(str(text), model_tipo_detencion)
+            if category_tipo == "VACIO":
+                category_tipo = ""
+            df.loc[idx, 'Tipo de Detención'] = category_tipo
+
+        # Condición 7: '5030'
+        if str(df.loc[idx, 'Número Reason']) == '5030':
+            print("Condición 7 se cumple")
             category_comentario = inference(str(text), model_comentario)
+            if category_comentario == "VACIO":
+                category_comentario = ""
             df.loc[idx, 'Comentario'] = category_comentario
+            df.loc[idx, 'Nivel_1'] = 'NEUMATICOS_Y_AROS'
+            df.loc[idx, 'Nivel_2'] = 'NEUMATICOS_Y_AROS'
+            df.loc[idx, 'Nivel_3'] = None
+            df.loc[idx, 'Tipo de Detención'] = 'MCV'
+
+        # Condición 8: '5050'
+        if str(df.loc[idx, 'Número Reason']) == '5050':
+            print("Condición 8 se cumple")
+            category_comentario = inference(str(text), model_comentario)
+            if category_comentario == "VACIO":
+                category_comentario = ""
+            df.loc[idx, 'Comentario'] = category_comentario
+            category_nivel_1 = inference(str(text), model_nivel_1)
+            if category_nivel_1 == "VACIO":
+                category_nivel_1 = ""
+            df.loc[idx, 'Nivel_1'] = category_nivel_1
+            category_nivel_2 = inference(str(text), model_nivel_2)
+            if category_nivel_2 == "VACIO":
+                category_nivel_2 = ""
+            df.loc[idx, 'Nivel_2'] = category_nivel_2
+            category_nivel_3 = inference(str(text), model_nivel_3)
+            if category_nivel_3 == "VACIO":
+                category_nivel_3 = ""
+            df.loc[idx, 'Nivel_3'] = category_nivel_3
             df.loc[idx, 'Tipo de Detención'] = 'MCM'
 
-        # Condición 7: '5070'
+        # Condición 9: '5070'
         if str(df.loc[idx, 'Número Reason']) == '5070':
-            print("Condición 7 se cumple")
+            print("Condición 9 se cumple")
+            category_comentario = inference(str(text), model_comentario)
+            if category_comentario == "VACIO":
+                category_comentario = ""
+            df.loc[idx, 'Comentario'] = category_comentario
             category_nivel_1 = inference(str(text), model_nivel_1)
             if category_nivel_1 == "VACIO":
                 category_nivel_1 = ""
@@ -173,28 +293,9 @@ if __name__ == "__main__":
             if category_nivel_3 == "VACIO":
                 category_nivel_3 = ""
             df.loc[idx, 'Nivel_3'] = category_nivel_3
-            category_comentario = inference(str(text), model_comentario)
-            df.loc[idx, 'Comentario'] = category_comentario
-            df.loc[idx, 'Tipo de Detención'] = 'MCI'
-
-        # Condición 8: '5000'
-        if str(df.loc[idx, 'Número Reason']) == '5000':
-            print("Condición 8 se cumple")
-            category_nivel_1 = inference(str(text), model_nivel_1)
-            if category_nivel_1 == "VACIO":
-                category_nivel_1 = ""
-            df.loc[idx, 'Nivel_1'] = category_nivel_1
-            category_nivel_2 = inference(str(text), model_nivel_2)
-            if category_nivel_2 == "VACIO":
-                category_nivel_2 = ""
-            df.loc[idx, 'Nivel_2'] = category_nivel_2
-            category_nivel_3 = inference(str(text), model_nivel_3)
-            if category_nivel_3 == "VACIO":
-                category_nivel_3 = ""
-            df.loc[idx, 'Nivel_3'] = category_nivel_3
-            category_comentario = inference(str(text), model_comentario)
-            df.loc[idx, 'Comentario'] = category_comentario
             category_tipo = inference(str(text), model_tipo_detencion)
+            if category_tipo == "VACIO":
+                category_tipo = ""
             df.loc[idx, 'Tipo de Detención'] = category_tipo
             
 
@@ -203,4 +304,7 @@ if __name__ == "__main__":
         os.makedirs('data')
 
     # Guardar el DataFrame en un archivo de Excel en la carpeta 'data'
-    df.to_excel('data/Resultados.xlsx', index=False)
+    save_path = save_results()
+    df.to_excel(save_path, index=False)
+
+## DEMORA 7 MINUTOS Y MEDIO EN CLASIFICAR 3233 DATOS
