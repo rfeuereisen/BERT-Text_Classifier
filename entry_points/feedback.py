b78f2c7bd23e8fb1c43fac94c55531cc1c9099f4
Rosario Feuereisen <112586014+rfeuereisen@users.noreply.github.com>
2023-12-06 09:50:30 -0300

    Update feedback.py

--- entry_points/feedback.py
+++ entry_points/feedback.py
@@ -2,6 +2,28 @@ import tkinter as tk
 from tkinter import simpledialog
 import pandas as pd
 
+# Función para mostrar la ventana emergente de clasificación correcta
+def mostrar_ventana_clasificacion_correcta():
+    # Crear una ventana emergente para ingresar las clasificaciones correctas
+    clasificacion_correcta_dialog = tk.Toplevel(root)
+    clasificacion_correcta_dialog.title("Clasificación correcta")
+
+    # Etiquetas y campos de entrada para cada categoría
+    categorias = ["Comentario", "Nivel_1", "Nivel_2", "Nivel_3", "Tipo de Detención"]
+    clasificacion_correcta_entries = {}
+
+    for categoria in categorias:
+        tk.Label(clasificacion_correcta_dialog, text=f"{categoria}:").pack()
+        entry = tk.Entry(clasificacion_correcta_dialog)
+        entry.pack()
+        clasificacion_correcta_entries[categoria] = entry
+
+    # Botón para confirmar las clasificaciones correctas
+    confirmar_button = tk.Button(clasificacion_correcta_dialog, text="Confirmar", command=clasificacion_correcta_dialog.destroy)
+    confirmar_button.pack()
+
+    return clasificacion_correcta_entries, clasificacion_correcta_dialog
+
 # Crea una ventana principal
 root = tk.Tk()
 root.withdraw()  # Oculta la ventana principal
@@ -15,23 +37,26 @@ clasificaciones_incorrectas = []
 # Bucle para revisar las clasificaciones incorrectas
 while True:
     respuesta = simpledialog.askstring("Clasificación incorrecta", "¿Hubo alguna clasificación incorrecta? (Sí/No)")
-    
+
     if respuesta.lower() != 'si':
         break  # Sal del bucle si la respuesta no es 'Sí'
-    
+
     dato_incorrecto = simpledialog.askstring("Dato incorrecto", "¿Qué dato fue mal clasificado?")
-    
-    # Pregunta por la clasificación correcta para cada categoría
-    clasificacion_correcta = {}
-    for categoria in ["Comentario", "Nivel_1", "Nivel_2", "Nivel_3", "Tipo de Detención"]:
-        clasificacion = simpledialog.askstring("Clasificación correcta", f"¿Cuál es la clasificación correcta para {categoria}?")
-        clasificacion_correcta[categoria] = clasificacion
-    
+
+    # Mostrar la ventana emergente de clasificación correcta
+    clasificacion_correcta_entries, clasificacion_correcta_dialog = mostrar_ventana_clasificacion_correcta()
+
+    # Obtener las clasificaciones correctas ingresadas por el usuario
+    clasificacion_correcta = {categoria: entry.get() for categoria, entry in clasificacion_correcta_entries.items()}
+
     # Almacena la información en la lista de clasificaciones incorrectas
     clasificaciones_incorrectas.append({
         "Dato incorrecto": dato_incorrecto,
         "Clasificación correcta": clasificacion_correcta
     })
+
+    # Cerrar la ventana de clasificación correcta
+    clasificacion_correcta_dialog.wait_window()
 
 # Guarda las clasificaciones incorrectas en un archivo Excel
 if len(clasificaciones_incorrectas) > 0:
@@ -39,5 +64,5 @@ if len(clasificaciones_incorrectas) > 0:
     feedback_df.to_excel('data/feedback.xlsx', index=False)
     print("Clasificaciones incorrectas guardadas en feedback.xlsx")
 
-# Cierra la ventana emergente
+# Cierra la ventana principal
 root.destroy()
